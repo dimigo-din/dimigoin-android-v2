93bd3d6233b0d7089531e8e393e897c400c5c9c8
Beomjun Kim <nutyworks@gmail.com>
2021-12-30 16:26:28 +0900

    Setup light theme

--- app/src/main/java/in/dimigo/dimigoin/ui/MainActivity.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/MainActivity.kt
@@ -1,38 +1,20 @@
 package `in`.dimigo.dimigoin.ui
 
+import `in`.dimigo.dimigoin.ui.theme.DimigoinTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import `in`.dimigo.dimigoin.ui.theme.DimigoinTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DimigoinTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DimigoinTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/in/dimigo/dimigoin/ui/theme/Type.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/theme/Type.kt
@@ -1,28 +1,56 @@
 package `in`.dimigo.dimigoin.ui.theme
 
+import `in`.dimigo.dimigoin.R
+import androidx.compose.material.Text
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
+val AppleSDGothicNeoFontFamily = FontFamily(
+    Font(R.font.b, weight = FontWeight.Bold),
+    Font(R.font.eb, weight = FontWeight.ExtraBold),
+    Font(R.font.l, weight = FontWeight.Light),
+    Font(R.font.m, weight = FontWeight.Medium)
+)
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    defaultFontFamily = AppleSDGothicNeoFontFamily,
+    h1 = TextStyle(
+        fontWeight = FontWeight.W700,
+        fontSize = 24.sp,
+        lineHeight = 4.8.sp,
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    h2 = TextStyle(
+        fontWeight = FontWeight.W700,
+        fontSize = 22.sp,
+        lineHeight = 4.4.sp,
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.W700,
+        fontSize = 20.sp,
+        lineHeight = 4.sp,
+    ),
+    h4 = TextStyle(
+        fontWeight = FontWeight.W700,
+        fontSize = 18.sp,
+        lineHeight = 3.6.sp,
+    ),
+    body1 = TextStyle(
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+        lineHeight = 3.2.sp,
+    ),
+    body2 = TextStyle(
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        lineHeight = 11.sp,
+    ),
+    button = TextStyle(
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+        lineHeight = 2.8.sp,
+    ),
 )
