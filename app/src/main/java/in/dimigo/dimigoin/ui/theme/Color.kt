5fb77da8a51be8f3e0f3783a35b085670aecd7c9
Beomjun Kim <nutyworks@gmail.com>
2021-12-30 16:26:28 +0900

    Setup light theme

--- app/src/main/java/in/dimigo/dimigoin/ui/theme/Color.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package `in`.dimigo.dimigoin.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Grey250 = Color(0xFFE5E5E5)
+val GreyBlue = Color(0xFFB1B8C1)
+val Red = Color(0xFFED4455)
+val MagentaDimigo = Color(0xFFE83C77)
+val YellowLight = Color(0xFFFFB800)
--- app/src/main/java/in/dimigo/dimigoin/ui/theme/Theme.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/theme/Theme.kt
@@ -5,30 +5,30 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = MagentaDimigo,
+    background = Grey250,
+    onBackground = GreyBlue,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSurface = Color.Black,
+    error = Red,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
+    primary = MagentaDimigo,
+    background = Grey250,
+    onBackground = GreyBlue,
     surface = Color.White,
     onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+    error = Red,
 )
 
 @Composable
-fun DimigoinTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun DimigoinTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
