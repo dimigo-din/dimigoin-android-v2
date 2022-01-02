1f684a0522af9d209f0ed669b10501e53852e3c2
Beomjun Kim <nutyworks@gmail.com>
2022-01-02 22:39:43 +0900

    Add place use cases

---
+++ domain/src/main/java/in/dimigo/dimigoin/domain/usecase/SetCurrentPlaceUseCase.kt
@@ -0,0 +1,11 @@
+package `in`.dimigo.dimigoin.domain.usecase
+
+import `in`.dimigo.dimigoin.domain.entity.Place
+import `in`.dimigo.dimigoin.domain.repository.PlaceRepository
+
+class SetCurrentPlaceUseCase(
+    private val placeRepository: PlaceRepository,
+) {
+    suspend operator fun invoke(place: Place) = invoke(place._id)
+    suspend operator fun invoke(placeId: String) = placeRepository.setCurrentPlace(placeId)
+}
