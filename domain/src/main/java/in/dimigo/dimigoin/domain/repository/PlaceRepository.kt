0f96b56fe8bd5f4815b10a0f6dc856e67efd0455
Beomjun Kim <nutyworks@gmail.com>
2022-01-02 22:35:45 +0900

    Add place repository

---
+++ domain/src/main/java/in/dimigo/dimigoin/domain/repository/PlaceRepository.kt
@@ -0,0 +1,48 @@
+package `in`.dimigo.dimigoin.domain.repository
+
+import `in`.dimigo.dimigoin.domain.entity.Place
+
+interface PlaceRepository {
+
+    /**
+     * Get all available places.
+     *
+     * @return list of places
+     */
+    suspend fun getAllPlaces(): List<Place>
+
+    /**
+     * Set current place.
+     *
+     * @return true if the request was successful
+     */
+    suspend fun setCurrentPlace(placeId: String): Boolean
+
+    /**
+     * Get current place.
+     *
+     * @return current place
+     */
+    suspend fun getCurrentPlace(): Place
+
+    /**
+     * Mark the place as favorite.
+     *
+     * @return true if the request was successful
+     */
+    suspend fun addFavoritePlace(placeId: String): Boolean
+
+    /**
+     * Remove favorite from the place.
+     *
+     * @return true if the request was successful
+     */
+    suspend fun removeFavoritePlace(placeId: String): Boolean
+
+    /**
+     * Get all favorite places.
+     *
+     * @return favorite places
+     */
+    suspend fun getFavoritePlacesId(): List<String>
+}
