05950b690ea488dccb30fac19969ba46a121408d
Beomjun Kim <nutyworks@gmail.com>
2022-01-02 21:22:59 +0900

    Add josa-complete function (#14)

---
+++ domain/src/main/java/in/dimigo/dimigoin/domain/util/JosaComplete.kt
@@ -0,0 +1,23 @@
+package `in`.dimigo.dimigoin.domain.util
+
+fun String.josa(josa: String, onlyJosa: Boolean = false): String {
+
+    val lastChar = this.last().code
+    val batchim = lastChar.minus(0xAC00).mod(28)
+
+    return (if (onlyJosa) "" else this) + when (josa) {
+        "을를", "를을" -> {
+            if (batchim == None) "를" else "을"
+        }
+        "이가", "가이" -> {
+            if (batchim == None) "가" else "이"
+        }
+        "으로" -> {
+            if (batchim == None || batchim == Rieul) "로" else "으로"
+        }
+        else -> ""
+    }
+}
+
+private const val None = 0 /* 받침 없음 */
+private const val Rieul = 8 /* ㄹ */
