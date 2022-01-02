ad26049a11cde5b335727665db66252bb5bff83f
Beomjun Kim <nutyworks@gmail.com>
2022-01-02 22:14:29 +0900

    Add `Place` entity

---
+++ domain/src/main/java/in/dimigo/dimigoin/domain/entity/Place.kt
@@ -0,0 +1,10 @@
+package `in`.dimigo.dimigoin.domain.entity
+
+data class Place(
+    val _id: String,
+    val name: String,
+    val building: String,
+    val floor: String?,
+    val type: String,
+    val isFavorite: Boolean,
+)
