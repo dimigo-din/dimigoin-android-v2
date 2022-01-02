2542dee29362174986d7270e0aefde9fb69f212a
Beomjun Kim <nutyworks@gmail.com>
2022-01-02 17:31:56 +0900

    Add `BottomNavigation` composable

--- app/src/main/java/in/dimigo/dimigoin/ui/composables/BottomNavigation.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/composables/BottomNavigation.kt
@@ -1,69 +1,132 @@
 package `in`.dimigo.dimigoin.ui.composables
 
-import `in`.dimigo.dimigoin.ui.composables.modifiers.noRippleClickable
 import `in`.dimigo.dimigoin.ui.theme.C3
 import `in`.dimigo.dimigoin.ui.theme.DTypography
 import `in`.dimigo.dimigoin.ui.theme.Point
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentColor
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun BottomNavigation(
-
+    content: @Composable RowScope.() -> Unit,
 ) {
-    androidx.compose.material.BottomNavigation() {
-        
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .clip(RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)),
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 10.dp)
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            content()
+        }
     }
 }
 
 @Composable
-fun BottomNavigationItem(
+fun RowScope.BottomNavigationItem(
+    modifier: Modifier = Modifier,
     icon: @Composable () -> Unit,
     label: @Composable () -> Unit,
     selected: Boolean,
     onClick: () -> Unit,
-) = Column(
-    modifier = Modifier.noRippleClickable { onClick() },
-    horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.spacedBy(7.dp),
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    selectedColor: Color = Point,
+    unSelectedColor: Color = C3,
 ) {
-    val color = if (selected) Point else C3
+    val color = if (selected) selectedColor else unSelectedColor
+    val ripple = rememberRipple(bounded = false, color = color)
 
-    CompositionLocalProvider(LocalContentColor provides color) {
-        icon()
-        label()
+    Column(
+        modifier = modifier
+            .weight(1f)
+            .selectable(
+                selected,
+                role = Role.Tab,
+                onClick = onClick,
+                interactionSource = interactionSource,
+                indication = ripple,
+                enabled = true
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        CompositionLocalProvider(LocalContentColor provides color) {
+            icon()
+            label()
+        }
     }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF, name = "Bottom Navigation Bar Item (not selected)")
 @Composable
 fun BNItemPreview() {
-    BottomNavigationItem(
-        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
-        label = { Text("Favorite", style = DTypography.t6) },
-        selected = false,
-        onClick = { },
-    )
+    Row {
+        BottomNavigationItem(
+            icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+            label = { Text("Favorite", style = DTypography.t6) },
+            selected = false,
+            onClick = { },
+        )
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFF, name = "Bottom Navigation Bar Item (not selected)")
 @Composable
 fun BNItemPreviewSelected() {
-    BottomNavigationItem(
-        icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
-        label = { Text("Favorite", style = DTypography.t6) },
-        selected = true,
-        onClick = { },
-    )
+    Row {
+        BottomNavigationItem(
+            icon = { Icon(Icons.Filled.Favorite, contentDescription = null, modifier = Modifier.size(24.dp)) },
+            label = { Text("Favorite", style = DTypography.t6) },
+            selected = true,
+            onClick = { },
+        )
+    }
+}
+
+@Preview(showBackground = false, name = "Bottom Navigation Bar")
+@Composable
+fun BottomNavigationPreview() {
+    BottomNavigation {
+        listOf("메인", "급식", "일정", "신청", "내 정보").forEach {
+            BottomNavigationItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
+                label = { Text(it, style = DTypography.t6) },
+                selected = it == "급식",
+                onClick = { },
+            )
+        }
+    }
 }
