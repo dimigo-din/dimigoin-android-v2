43491522a33f157a7f9b53e1f865321121216844
Beomjun Kim <nutyworks@gmail.com>
2022-01-06 12:20:59 +0900

    Fix background color (#40)

--- app/src/main/java/in/dimigo/dimigoin/ui/theme/Color.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package `in`.dimigo.dimigoin.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Grey250 = Color(0xFFE5E5E5)
 val C1 = Color(0xFF505966)
 val C2 = Color(0xFFB1B8C1)
 val C3 = Color(0xFFD2D6DB)
