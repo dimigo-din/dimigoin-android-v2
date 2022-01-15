7188d8c21060532a4445556d722a98ffd3519b51
Beomjun Kim <nutyworks@gmail.com>
2022-01-15 17:53:07 +0900

    Add meal related models

---
+++ domain/src/main/java/in/dimigo/dimigoin/domain/entity/MealTime.kt
@@ -0,0 +1,14 @@
+package `in`.dimigo.dimigoin.domain.entity
+
+import java.time.LocalTime
+
+typealias MealTimes = HashMap<String, MealTime>
+
+data class MealTime(
+    val order: Int,
+    val grade: Int,
+    val `class`: Int,
+    val breakfastTime: LocalTime,
+    val lunchTime: LocalTime,
+    val dinnerTime: LocalTime,
+)
