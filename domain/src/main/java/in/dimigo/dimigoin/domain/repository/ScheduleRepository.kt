d2d279b67c397332dad2ef252b212a20a0b6224e
Beomjun Kim <nutyworks@gmail.com>
2022-02-24 20:14:23 +0900

    Mark functions suspend

--- domain/src/main/java/in/dimigo/dimigoin/domain/repository/ScheduleRepository.kt
+++ domain/src/main/java/in/dimigo/dimigoin/domain/repository/ScheduleRepository.kt
@@ -4,6 +4,6 @@ import `in`.dimigo.dimigoin.domain.entity.schedule.AnnualSchedule
 import `in`.dimigo.dimigoin.domain.entity.schedule.WeeklyTimetable
 
 interface ScheduleRepository {
-    fun getTimetableByClass(grade: Int, `class`: Int): Result<WeeklyTimetable>
-    fun getSchoolSchedule(): Result<AnnualSchedule>
+    suspend fun getTimetableByClass(grade: Int, `class`: Int): Result<WeeklyTimetable>
+    suspend fun getSchoolSchedule(): Result<AnnualSchedule>
 }
