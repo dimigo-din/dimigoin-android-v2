5ddee23d55ad0f74c6f94d4e916eb03109e965b6
Beomjun Kim <nutyworks@gmail.com>
2022-02-24 20:08:34 +0900

    Add timetable response model

---
+++ data/src/main/java/in/dimigo/dimigoin/data/model/schedule/WeeklyTimetableResponseModel.kt
@@ -0,0 +1,7 @@
+package `in`.dimigo.dimigoin.data.model.schedule
+
+import `in`.dimigo.dimigoin.domain.entity.schedule.WeeklyTimetable
+
+data class WeeklyTimetableResponseModel(
+    val timetable: WeeklyTimetable
+)
