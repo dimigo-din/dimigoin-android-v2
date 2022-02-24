904bad25b454302f4c99263f0ea66d4a3960a485
Beomjun Kim <nutyworks@gmail.com>
2022-02-24 17:08:40 +0900

    Add meal response models

---
+++ data/src/main/java/in/dimigo/dimigoin/data/model/meal/MealSequenceResponseModel.kt
@@ -0,0 +1,6 @@
+package `in`.dimigo.dimigoin.data.model.meal
+
+data class MealSequenceResponseModel(
+    val lunch: List<List<Int>>,
+    val dinner: List<List<Int>>,
+)
---
+++ data/src/main/java/in/dimigo/dimigoin/data/model/meal/MealTimeResponseModel.kt
@@ -0,0 +1,6 @@
+package `in`.dimigo.dimigoin.data.model.meal
+
+data class MealTimeResponseModel(
+    val lunch: List<List<Int>>,
+    val dinner: List<List<Int>>,
+)
