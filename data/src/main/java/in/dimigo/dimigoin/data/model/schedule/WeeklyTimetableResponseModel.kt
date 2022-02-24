d61bfe0b1ffde265f4c3f1222f0fd0fa1700eb3d
Beomjun Kim <nutyworks@gmail.com>
2022-02-24 20:20:14 +0900

    Change timetable response model

--- data/src/main/java/in/dimigo/dimigoin/data/model/schedule/WeeklyTimetableResponseModel.kt
+++ data/src/main/java/in/dimigo/dimigoin/data/model/schedule/WeeklyTimetableResponseModel.kt
@@ -1,7 +1,10 @@
 package `in`.dimigo.dimigoin.data.model.schedule
 
-import `in`.dimigo.dimigoin.domain.entity.schedule.WeeklyTimetable
-
 data class WeeklyTimetableResponseModel(
-    val timetable: WeeklyTimetable
-)
+    val timetable: List<Timetable>
+) {
+    data class Timetable(
+        val sequence: List<String?>,
+        val date: String,
+    )
+}
