5922047eeaa985266513255bfdb7eece7ded5f4d
gijuno <itx.ljh.developer@gmail.com>
2022-03-07 21:20:15 +0900

    feat: build ApplicationScreen

--- app/src/main/java/in/dimigo/dimigoin/ui/MainActivity.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/MainActivity.kt
@@ -7,6 +7,7 @@ import `in`.dimigo.dimigoin.ui.composables.BottomNavigation
 import `in`.dimigo.dimigoin.ui.composables.BottomNavigationItem
 import `in`.dimigo.dimigoin.ui.composables.CustomSnackbarHost
 import `in`.dimigo.dimigoin.ui.composables.CustomSnackbarHostState
+import `in`.dimigo.dimigoin.ui.screen.ApplicationScreen
 import `in`.dimigo.dimigoin.ui.screen.LoginScreen
 import `in`.dimigo.dimigoin.ui.screen.MainScreen
 import `in`.dimigo.dimigoin.ui.screen.ScheduleScreen
@@ -227,7 +228,7 @@ fun App(
                 )
             }
             composable(Screen.Calendar.route) { ScheduleScreen() }
-            composable(Screen.Application.route) { Text(text = "신청") }
+            composable(Screen.Application.route) { ApplicationScreen() }
             composable(Screen.MyInfo.route) { Text(text = "내 정보") }
             placeSelectorNavGraph(navController, onPlaceChange, onFavoriteAdd, onFavoriteRemove)
         }
